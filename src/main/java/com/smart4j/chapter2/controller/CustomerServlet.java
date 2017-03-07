3dbb2b67814f46e2e702dd2bc52441e9cfbe6fd0
lizanle <491823689@qq.com>
2017-03-07 22:58:04 +0800

    servlet jsp页面完善

--- src/main/java/com/smart4j/chapter2/controller/CustomerServlet.java
+++ src/main/java/com/smart4j/chapter2/controller/CustomerServlet.java
@@ -1,17 +1,27 @@
 package com.smart4j.chapter2.controller;
 
+import com.smart4j.chapter2.model.Customer;
+import com.smart4j.chapter2.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/3/3.
  */
 @WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
+    private CustomerService customerService;
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
     /**
      * 进入创建客户的界面
      * @param req
@@ -21,8 +31,11 @@ public class CustomerServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<Customer> list = customerService.getCustomerList();
+        req.setAttribute("customerList",list);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req,resp);
     }
+
 
     /**
      * 处理创建客户的请求
